9dc17846921d61a4fcf7e94ae0fc4be30df0550a
JamesShabo <jamesshabo@Jamess-MacBook-Pro.local>
2016-10-10 00:08:53 -0700

    Weekend Work

---
+++ etsypage.js
@@ -0,0 +1,40 @@
+$.getJSON( "https://api.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop", function( data ) {
+    console.log(data)
+    putInDom(data)
+});
+
+
+function putInDom(data) {
+    var ourData = data.results.map(function(item){
+        return {
+            description: item.description,
+            price: item.price,
+            link: item.url,
+            title: item.title,
+            img: item.Images[0].url_570xN,
+            shopName: item.Shop.shop_name
+        }
+    })
+
+    var htmlStr = "";
+    ourData.forEach(function(item){
+        htmlStr += `
+        <div class="galleryItem">
+            <a href=${item.link}> 
+            <div><img src="${item.img}"/></div>
+            <p class="truncate galleryDescription"> ${item.title} </p>
+            <div class="galleryFooter">
+                <p class="galleryShop"> ${item.shopName} </p>
+                <p class="galleryPrice"> $${item.price} </p>
+            </div>
+            </a>
+            <div class="collections-ui">
+                <div class="favorite-container"></div>
+                <div class="collect-container"></div>
+            </div>
+        </div>
+        `
+    })
+
+    $("#galleryDisplay").html(htmlStr)
+}
